0c1b14994e52cb8440f7d2aef2837896ddc2b9ef
Aditya Nashikkar <Aditya.Nashikkar@niceactimize.com>
2021-06-16 15:25:29 +0530

    dynamic content added in take action

--- src/app/components/take-action/take-action.component.ts
+++ src/app/components/take-action/take-action.component.ts
@@ -13,11 +13,17 @@ export class TakeActionComponent implements OnInit {
   infoSectionHeader = 'How does your carbon footprint compare?​'
   infoSectionMain = ['Plastic surrounds us and is part of everything we do. From our clothes to our cars, packaging for everything from our lunch to our online orders, plastic is in every room of our homes. It is even in our walls.', 'Our Carbon Footprint Calculator looks beyond single use plastics/non-degradable items to help us understand the full story of plastics and  in our lives and the impact of our choices.'];
   userQuestion: Array<UserQuestion> = takeActionConstant.USER_QUESTIONS;
+  selectedQuestionNumber = 3;
+  selectedQuestion = this.userQuestion[this.selectedQuestionNumber];
 
   constructor() { }
 
   ngOnInit(): void {
-    console.log(this.userQuestion);
+    console.log(this.selectedQuestion);
   }
+
+  // getQuestionByNumber(questionNumber: number) {
+
+  // }
 
 }
