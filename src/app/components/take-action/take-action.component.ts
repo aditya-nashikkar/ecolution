3a6af5455fef5eaacfcb20dd35ee6c9301409065
Aditya Nashikkar <Aditya.Nashikkar@niceactimize.com>
2021-06-16 16:37:38 +0530

    take-action done UI

--- src/app/components/take-action/take-action.component.ts
+++ src/app/components/take-action/take-action.component.ts
@@ -13,7 +13,7 @@ export class TakeActionComponent implements OnInit {
   infoSectionHeader = 'How does your carbon footprint compare?​'
   infoSectionMain = ['Plastic surrounds us and is part of everything we do. From our clothes to our cars, packaging for everything from our lunch to our online orders, plastic is in every room of our homes. It is even in our walls.', 'Our Carbon Footprint Calculator looks beyond single use plastics/non-degradable items to help us understand the full story of plastics and  in our lives and the impact of our choices.'];
   userQuestion: Array<UserQuestion> = takeActionConstant.USER_QUESTIONS;
-  selectedQuestionNumber = 3;
+  selectedQuestionNumber = 0;
   selectedQuestion = this.userQuestion[this.selectedQuestionNumber];
 
   constructor() { }
@@ -22,8 +22,13 @@ export class TakeActionComponent implements OnInit {
     console.log(this.selectedQuestion);
   }
 
-  // getQuestionByNumber(questionNumber: number) {
-
-  // }
+  getQuestionByNumber(step: string) {
+    if (step === 'next') {
+      this.selectedQuestionNumber += 1;
+    } else if (step === 'previous') {
+      this.selectedQuestionNumber -= 1;
+    }
+    this.selectedQuestion = this.userQuestion[this.selectedQuestionNumber];
+  }
 
 }
